8e054c1628e54015394bd78d1af6cf380f1bcf3a
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-05-19 15:57:06 +0530

    A basic styling of all pages are done using bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random()*20)+10
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image:'http://source.unsplash.com/collection/483251',
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti eligendi hic assumenda quisquam itaque voluptatibus ex enim consequuntur illum molestias dolores porro exercitationem, tempore nemo quia doloremque repellat, incidunt minima",
+      price
     });
     await camp.save();
   }
