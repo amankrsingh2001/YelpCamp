3c4188c7b37aba7319af7e79f5fdf32c2eb04ce2
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-05-19 22:30:47 +0530

    Basic Error handling and ExpressError class defined and CatchAsync funciton defined

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
+const CatchAsync = require('./utils/CatchAsync')
 const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -39,34 +40,41 @@ app.get('/campgrounds/new',(req,res)=>{
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds',async (req,res)=>{
-const campground = new Campground(req.body.campground)
-await campground.save();
-res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post('/campgrounds',CatchAsync( async (req,res,next)=>{
+    const campground = new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
 
-app.get('/campgrounds/:id',async(req,res)=>{
+app.get('/campgrounds/:id', CatchAsync( async(req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground})
-})
+}))
 
-app.get('/campgrounds/:id/edit',async (req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit',{campground})
-})
+app.get('/campgrounds/:id/edit', CatchAsync(async (req,res)=>{
 
-app.put('/campgrounds/:id',async (req,res)=>{
+        const campground = await Campground.findById(req.params.id)
+        res.render('campgrounds/edit',{campground})
+   
+}))
+
+app.put('/campgrounds/:id', CatchAsync(async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id',async (req,res)=>{
+app.delete('/campgrounds/:id',CatchAsync(async (req,res)=>{
     const {id} = req.params
      await Campground.findByIdAndDelete(id)
      res.redirect('/campgrounds')
+}))
+
+app.use((err,req,res,next)=>{
+    res.status(500).send("Oh boy,Something went wrong")
 })
 
 app.listen(3000,()=>{
     console.log('App is listining on Port 3000');
 })
+
