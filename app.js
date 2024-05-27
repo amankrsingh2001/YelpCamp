063128e7edcafa7e1a78f3dc5f241dd0b33c4683
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-05-28 02:16:31 +0530

    Breaking out Review routers

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
-const Joi = require('joi')
-const {campgroundSchema,reviewSchema} = require('./schemas.js')
-const Campground = require('./models/campground')
-const CatchAsync = require('./utils/CatchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
-const Review = require('./models/review')
+
 
 const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -32,41 +31,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
 
-
-const validateReview = (req,res,next) =>{
-    const {error} = reviewSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg,400)
-    }else{
-        next()
-    }
-    
-}
-
 app.use('/campgrounds',campgrounds)
+app.use('/campgrounds/:id/reviews',reviews)
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
-
-
-app.post('/campgrounds/:id/reviews',validateReview,CatchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId',CatchAsync(async(req,res)=>{
-    const {id,reviewId} = req.params
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(req.params.reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
-
 
 
 app.all("*",(req,res,next)=>{  //for every path we are going to call this call back
