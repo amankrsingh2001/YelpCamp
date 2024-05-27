80ecaff633f50960e1241cbcdf69903e164be8d1
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-05-28 02:31:51 +0530

    serving static assets

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const methodOverride = require('method-override')
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
-
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
-
 //database connetion 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,'connection error'))
@@ -25,19 +22,19 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
 
-
 app.engine('ejs',ejsMate)
+
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/campgrounds',campgrounds)
 app.use('/campgrounds/:id/reviews',reviews)
 
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
-
 
 app.all("*",(req,res,next)=>{  //for every path we are going to call this call back
     next(new ExpressError('Page Not Found',404))
@@ -48,10 +45,7 @@ app.use((err,req,res,next)=>{
     const {statusCode = 500}  = err
     if(!err.message) err.message = "Something went wrong"
     res.status(statusCode).render('error',{err})
-
 })
-
-
 
 app.listen(3000,()=>{
     console.log('App is listining on Port 3000');
