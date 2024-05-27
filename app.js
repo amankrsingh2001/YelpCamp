1c1db1a689f0bf88a8c7c956fecb6d5afa518996
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-05-28 02:46:07 +0530

    configuring session

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
+
+const app = express();
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 //database connetion 
 const db = mongoose.connection;
@@ -17,7 +20,20 @@ db.once('open',()=>{
     console.log('Database Connected')
 })
 
-const app = express();
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave:false,
+    saveUninitialized :true,
+    cookie: {
+        httpOnly: true,
+        expires:Date.now() + 1000*60*60*24*7,
+        maxAge:1000*60*60*24*7
+    }
+}
+
+
+app.use(session(sessionConfig))
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
