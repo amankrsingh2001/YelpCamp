217c33f51fd2dd8affc7a91876d1d0ca244bde1c
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-06-01 23:23:55 +0530

    navbar

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ passport.deserializeUser(User.deserializeUser())
 
 
 app.use((req,res,next)=>{
-    res.locals.currentUser = req.user;
+    res.locals.currentUser = req.user ;
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     next()
