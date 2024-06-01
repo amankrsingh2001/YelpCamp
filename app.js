5159c416a24796d91a71599612db99607bdec980
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-06-01 18:01:21 +0530

    Fixed all the common values

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ if(process.env.NODE_ENV != "production"){
 }
 
 
+
+
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
@@ -15,16 +17,15 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
-
-
-
+const helmet = require('helmet')
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
+const MongoDBStore = require('connect-mongo')
+const dbUrl ='mongodb://localhost:27017/yelp-camp'
 
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect( process.env.DB_URL || dbUrl)
 //database connetion 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,'connection error'))
@@ -32,15 +33,31 @@ db.once('open',()=>{
     console.log('Database Connected')
 })
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const app = express();
 
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on('error',function(e){
+    console.log('Session store Error',e)
+})
+
 const sessionConfig = {
+    store,
+    name:'session',
     secret: 'thisshouldbeabettersecret!',
     resave:false,
     saveUninitialized :true,
     cookie: {
         httpOnly: true,
+        // secure:true,
         expires:Date.now() + 1000*60*60*24*7,
         maxAge:1000*60*60*24*7
     }
@@ -49,6 +66,53 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet())
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+    "https://cdn.jsdelivr.net", 
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dzlvmbajq/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -70,7 +134,6 @@ app.use(mongoSanitize({
 }))
 
 app.use((req,res,next)=>{
-    console.log(req.query)
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
@@ -104,7 +167,9 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err})
 })
 
-app.listen(3000,()=>{
-    console.log('App is listining on Port 3000');
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`App is listining on ${port}`);
 })
 
